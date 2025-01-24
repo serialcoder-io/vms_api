4836cc62ab6ab8e177c7251acd0a452264697be8
Anli omar <omaranli56@gmail.com>
2025-01-24 21:43:59 +0400

    feat: added vms_api/ for api root

--- vms_app/urls.py
+++ vms_app/urls.py
@@ -1,10 +1,11 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
-
-from rest_framework.routers import DefaultRouter
 from .views import (
     UserViewSet,
     UserRegisterView,
@@ -23,13 +24,12 @@ router.register('voucher_requests', VoucherRequestViewSet)
 router.register('companies', CompanyViewSet)
 router.register('shops', ShopViewSet)
 
-from django.urls import path, include
-from . import views
+app_name = 'vms_app'
 urlpatterns = [
     path('', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('register/', UserRegisterView.as_view(), name='register'),
+    path('api/register/', UserRegisterView.as_view(), name='register'),
     path('latest_id/', views.get_latest_id, name="latest_id")
 ]
