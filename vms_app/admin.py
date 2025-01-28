694cb8377f41c8c4e7e9b60d8414451fe4eb9c0c
Anli omar <omaranli56@gmail.com>
2025-01-28 09:14:54 +0400

    created class UserAdmin and registred it

--- vms_app/admin.py
+++ vms_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import VoucherRequest, Voucher, Client
+from .models import VoucherRequest, Voucher, Client, User
 
 class VoucherRequestAdmin(admin.ModelAdmin):
     list_display = ['request_ref', 'date_time_recorded', 'quantity_of_vouchers']
@@ -16,10 +16,16 @@ class VoucherRequestAdmin(admin.ModelAdmin):
 class VoucherAdmin(admin.ModelAdmin):
     list_display = ['voucher_ref', 'date_time_created', 'amount']
 
+
 class ClientAdmin(admin.ModelAdmin):
     list_display = ['firstname', 'lastname', 'email']
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
 
 
 admin.site.register(VoucherRequest, VoucherRequestAdmin)
 admin.site.register(Voucher, VoucherAdmin)
 admin.site.register(Client, ClientAdmin)
+admin.site.register(User, UserAdmin)
