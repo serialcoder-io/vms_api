07556e5beb0e1509defc185f38190f5f4752ce70
Omar.py <omaranli56@gmail.com>
2025-02-24 19:35:53 +0400

    feat: removed django-daisy then installed and added jazzmin to requirements.txt

--- vms_api/settings.py
+++ vms_api/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = [
 
 # Application definition
 INSTALLED_APPS = [
-    'django_daisy',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -149,6 +149,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
