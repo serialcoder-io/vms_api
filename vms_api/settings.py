0e758e46f9a37190bc5deb57f268d031ea70c0ee
Omar.py <omaranli56@gmail.com>
2025-03-04 15:43:55 +0400

    feat: use 'rest_framework.renderers.JSONRenderer' in DEFAULT_RENDERER_CLASSES on prod

--- vms_api/settings.py
+++ vms_api/settings.py
@@ -206,9 +206,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    #DEFAULT_RENDERER_CLASSES': [
-         #'rest_framework.renderers.JSONRenderer',
-    #],
+    'DEFAULT_RENDERER_CLASSES': [
+         'rest_framework.renderers.JSONRenderer',
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
