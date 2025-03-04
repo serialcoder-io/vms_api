5bf6a751e644a4761e7484225d499b92f0cd1bc6
Omar.py <omaranli56@gmail.com>
2025-03-04 14:17:20 +0400

    stye: separate the spninner to sumbmit button, in reset password form

--- staticfiles/js/reset-password.js
+++ staticfiles/js/reset-password.js
@@ -66,15 +66,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
     resetPwdForm.addEventListener("submit", async(event)=>{
         event.preventDefault()
         const isPasswordValid = validatePassword(passwordField.value, confirmPasswordField.value)
-        spinner.classList.add("d-none");
         if (!isPasswordValid) {
             return;
         }
-        spinner.classList.remove("d-none");
         const uid = document.getElementById("uid").value;
         const token = document.getElementById("token").value;
 
         try {
+            spinner.classList.remove("d-none");
             const resetPwd = await resetPassword(passwordField.value.trim(), uid, token);
             if (resetPwd.detail) {
                 alert(resetPwd.detail);
